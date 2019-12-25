070568ba654d9a0cd361a43001f8f4899888e33c
Bob James <bob@mobidawg.com>
2019-12-25 15:13:24 -0500

    Refactor flxOverlayBrowser 2 flxOverlaySomething

--- controllers/desktop/frmLandingController/frmLandingController.js
+++ controllers/desktop/frmLandingController/frmLandingController.js
@@ -29,60 +29,60 @@ define({
   
   onClickBrowser: function() {
     kony.print("*** CLICK ***");
-    //     if (this.view.flxOverlayBrowser.flxFrameBrowser.isVisible) {
-    //       this.view.flxOverlayBrowser.flxFrameBrowser.setVisibility(false);
+    //     if (this.view.flxOverlaySomething.flxFrameBrowser.isVisible) {
+    //       this.view.flxOverlaySomething.flxFrameBrowser.setVisibility(false);
     //     } else {
-    //       this.view.flxOverlayBrowser.flxFrameBrowser.setVisibility(true);
+    //       this.view.flxOverlaySomething.flxFrameBrowser.setVisibility(true);
     //     }
 
     if (this.view.info.browserActive.active) {
       this.view.info.browserActive.active = false;
-      this.view.flxOverlayBrowser.flxFrameBrowser.setVisibility(false);
+      this.view.flxOverlaySomething.flxFrameBrowser.setVisibility(false);
 
       let optionButtonReposition = 4;
       // Button 
       switch (optionButtonReposition) {
         case 1:
-// close ... mimics the original          this.view.flxOverlayBrowser.btnOpen.left =  45.0/100.0 * 90.0/100.0 + "%";
-//           this.view.flxOverlayBrowser.btnOpen.top = 80.0/100.0 * 90.0/100.0 + "%";
-          this.view.flxOverlayBrowser.btnOpen.left = 40.0/100.0 * 90.0/100.0 + "%"; // this.stateStore.parent.frame.left * 40.0/100.0 * 90.0/100.0 + "px";
-          this.view.flxOverlayBrowser.btnOpen.top = 80.0/100.0 * 90.0/100.0 + "%";
+// close ... mimics the original          this.view.flxOverlaySomething.btnOpen.left =  45.0/100.0 * 90.0/100.0 + "%";
+//           this.view.flxOverlaySomething.btnOpen.top = 80.0/100.0 * 90.0/100.0 + "%";
+          this.view.flxOverlaySomething.btnOpen.left = 40.0/100.0 * 90.0/100.0 + "%"; // this.stateStore.parent.frame.left * 40.0/100.0 * 90.0/100.0 + "px";
+          this.view.flxOverlaySomething.btnOpen.top = 80.0/100.0 * 90.0/100.0 + "%";
           break;
         case 2:
-          this.view.flxOverlayBrowser.btnOpen.left = this.stateStore.parent.frame.width * 40.0/100.0 * 90.0/100.0 + "px";
-          this.view.flxOverlayBrowser.btnOpen.top = "0px"; //this.view.flxOverlayBrowser.frame.height - (this.view.flxOverlayBrowser.frame.height * 80.0/100.0 * 90.0/100.0) + "px";
+          this.view.flxOverlaySomething.btnOpen.left = this.stateStore.parent.frame.width * 40.0/100.0 * 90.0/100.0 + "px";
+          this.view.flxOverlaySomething.btnOpen.top = "0px"; //this.view.flxOverlaySomething.frame.height - (this.view.flxOverlaySomething.frame.height * 80.0/100.0 * 90.0/100.0) + "px";
           break;
         case 3:
-//           this.view.flxOverlayBrowser.btnOpen.left = 60.0/100.0 * 90.0/100.0 + "%";
-//           this.view.flxOverlayBrowser.btnOpen.top = "0px";
-          this.view.flxOverlayBrowser.btnOpen.left = this.view.flxOverlayBrowser.width - this.view.flxOverlayBrowser.btnOpen.width + "px";
-          this.view.flxOverlayBrowser.btnOpen.top = this.view.flxOverlayBrowser.height - this.view.flxOverlayBrowser.btnOpen.height + "px";
+//           this.view.flxOverlaySomething.btnOpen.left = 60.0/100.0 * 90.0/100.0 + "%";
+//           this.view.flxOverlaySomething.btnOpen.top = "0px";
+          this.view.flxOverlaySomething.btnOpen.left = this.view.flxOverlaySomething.width - this.view.flxOverlaySomething.btnOpen.width + "px";
+          this.view.flxOverlaySomething.btnOpen.top = this.view.flxOverlaySomething.height - this.view.flxOverlaySomething.btnOpen.height + "px";
           break;
         case 4:
-          this.view.flxOverlayBrowser.btnOpen.left = 0 + "%";
-          this.view.flxOverlayBrowser.btnOpen.top = 0 + "%";
+          this.view.flxOverlaySomething.btnOpen.left = 0 + "%";
+          this.view.flxOverlaySomething.btnOpen.top = 0 + "%";
           break;
       }
       // Frame
-      this.view.flxOverlayBrowser.top = (80.0 + (0.0/2)/ this.stateStore.parent.frame.height).toFixed(1) + "%";  // " 90%";
-      this.view.flxOverlayBrowser.left = (80.0 + (0.0/2)/ this.stateStore.parent.frame.width).toFixed(1) + "%"; //"90%";
-      this.view.flxOverlayBrowser.height = this.view.flxOverlayBrowser.btnOpen.frame.height + "px";
-      this.view.flxOverlayBrowser.width = this.view.flxOverlayBrowser.btnOpen.frame.width + "px";
+      this.view.flxOverlaySomething.top = (80.0 + (0.0/2)/ this.stateStore.parent.frame.height).toFixed(1) + "%";  // " 90%";
+      this.view.flxOverlaySomething.left = (80.0 + (0.0/2)/ this.stateStore.parent.frame.width).toFixed(1) + "%"; //"90%";
+      this.view.flxOverlaySomething.height = this.view.flxOverlaySomething.btnOpen.frame.height + "px";
+      this.view.flxOverlaySomething.width = this.view.flxOverlaySomething.btnOpen.frame.width + "px";
     } else {
       this.view.info.browserActive.active = true;
       // Frame
-      this.view.flxOverlayBrowser.setVisibility(true);
-      this.view.flxOverlayBrowser.flxFrameBrowser.setVisibility(true);
-      this.view.flxOverlayBrowser.flxFrameBrowser.browser.setVisibility(true);
-      this.view.flxOverlayBrowser.top = this.stateStore.parent.frame.height - this.stateStore.parent.frame.height * 80.0/100.0 - this.view.flxOverlayBrowser.btnOpen.frame.height + "px";
-      this.view.flxOverlayBrowser.left = this.stateStore.parent.frame.width * 40.0/100.0 + "px";
-      this.view.flxOverlayBrowser.height = this.stateStore.parent.frame.height * 80.0/100.0 + "px";
-      this.view.flxOverlayBrowser.width = "40%";
+      this.view.flxOverlaySomething.setVisibility(true);
+      this.view.flxOverlaySomething.flxFrameBrowser.setVisibility(true);
+      this.view.flxOverlaySomething.flxFrameBrowser.browser.setVisibility(true);
+      this.view.flxOverlaySomething.top = this.stateStore.parent.frame.height - this.stateStore.parent.frame.height * 80.0/100.0 - this.view.flxOverlaySomething.btnOpen.frame.height + "px";
+      this.view.flxOverlaySomething.left = this.stateStore.parent.frame.width * 40.0/100.0 + "px";
+      this.view.flxOverlaySomething.height = this.stateStore.parent.frame.height * 80.0/100.0 + "px";
+      this.view.flxOverlaySomething.width = "40%";
       // Whaaaa ... i hope this isn't necssary, but you never know if there are order dependencies.
       this.view.forceLayout();
       // Button 
-      this.view.flxOverlayBrowser.btnOpen.left = 80 + "%"; // this.view.flxOverlayBrowser.frame.width - this.view.flxOverlayBrowser.btnOpen.frame.width + "px";
-      this.view.flxOverlayBrowser.btnOpen.frame.top = 55;
+      this.view.flxOverlaySomething.btnOpen.left = 80 + "%"; // this.view.flxOverlaySomething.frame.width - this.view.flxOverlaySomething.btnOpen.frame.width + "px";
+      this.view.flxOverlaySomething.btnOpen.frame.top = 55;
     }
     this.view.forceLayout();
   },
@@ -94,26 +94,26 @@ define({
 //     if (this.view.info.browserActive.active) {
 // 		this.view.info.browserActive.active = false;
 //         // Button 
-// //         this.view.flxOverlayBrowser.btnOpen.left = this.view.flxOverlayBrowser.frame.width * 45.0/100.0 * 90.0/100.0 + "px";
-// //         this.view.flxOverlayBrowser.btnOpen.top = this.view.flxOverlayBrowser.frame.height - (this.view.flxOverlayBrowser.frame.height * 80.0/100.0 * 90.0/100.0) + "px";
+// //         this.view.flxOverlaySomething.btnOpen.left = this.view.flxOverlaySomething.frame.width * 45.0/100.0 * 90.0/100.0 + "px";
+// //         this.view.flxOverlaySomething.btnOpen.top = this.view.flxOverlaySomething.frame.height - (this.view.flxOverlaySomething.frame.height * 80.0/100.0 * 90.0/100.0) + "px";
 //         // Frame
-//       	this.view.flxOverlayBrowser.top = "90%";
-//       	this.view.flxOverlayBrowser.left = "90%";
-//         this.view.flxOverlayBrowser.height = this.view.flxOverlayBrowser.btnOpen.frame.height + "px";
-//         this.view.flxOverlayBrowser.width = this.view.flxOverlayBrowser.btnOpen.frame.width + "px";
+//       	this.view.flxOverlaySomething.top = "90%";
+//       	this.view.flxOverlaySomething.left = "90%";
+//         this.view.flxOverlaySomething.height = this.view.flxOverlaySomething.btnOpen.frame.height + "px";
+//         this.view.flxOverlaySomething.width = this.view.flxOverlaySomething.btnOpen.frame.width + "px";
 //       } else {
 // 		this.view.info.browserActive.active = true;
 //         // Button 
-// //         this.view.flxOverlayBrowser.btnOpen.left = "0px";
-// //         this.view.flxOverlayBrowser.btnOpen.top = "0px";
+// //         this.view.flxOverlaySomething.btnOpen.left = "0px";
+// //         this.view.flxOverlaySomething.btnOpen.top = "0px";
 //         // Frame
-//         this.view.flxOverlayBrowser.setVisibility(true);
-//         this.view.flxOverlayBrowser.flxFrameBrowser.setVisibility(true);
-//         this.view.flxOverlayBrowser.flxFrameBrowser.browser.setVisibility(true);
-//       	this.view.flxOverlayBrowser.top = this.stateStore.parent.frame.height - this.stateStore.parent.frame.height * 80.0/100.0 - this.stateStore.paddingPx - this.view.flxOverlayBrowser.btnOpen.frame.height + "px";
-//       	this.view.flxOverlayBrowser.left = this.stateStore.parent.frame.width * 45.0/100.0 + "px";
-//         this.view.flxOverlayBrowser.height = this.stateStore.parent.frame.height * 80.0/100.0 + "px";
-//         this.view.flxOverlayBrowser.width = this.stateStore.parent.frame.width * 40.0/100.0 + "px";
+//         this.view.flxOverlaySomething.setVisibility(true);
+//         this.view.flxOverlaySomething.flxFrameBrowser.setVisibility(true);
+//         this.view.flxOverlaySomething.flxFrameBrowser.browser.setVisibility(true);
+//       	this.view.flxOverlaySomething.top = this.stateStore.parent.frame.height - this.stateStore.parent.frame.height * 80.0/100.0 - this.stateStore.paddingPx - this.view.flxOverlaySomething.btnOpen.frame.height + "px";
+//       	this.view.flxOverlaySomething.left = this.stateStore.parent.frame.width * 45.0/100.0 + "px";
+//         this.view.flxOverlaySomething.height = this.stateStore.parent.frame.height * 80.0/100.0 + "px";
+//         this.view.flxOverlaySomething.width = this.stateStore.parent.frame.width * 40.0/100.0 + "px";
 //       }
 //     this.view.forceLayout();
 //   },
